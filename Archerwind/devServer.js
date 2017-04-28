254dca85b0fdd2a15f35038fcb87d4b733257cb3
vanitax <anger637976@gmail.com>
2017-04-27 19:12:19 -0700

    fixed server config

--- Archerwind/devServer.js
+++ Archerwind/devServer.js
@@ -7,8 +7,7 @@ var
     webpack = require('webpack'),
     config = require('./webpack.config.dev.js'),
     webpackDevMiddleware = require('webpack-dev-middleware'),
-    dotenv = require('dotenv').config(),
-    port = dotenv.DEV_SERVER_PORT, // 記得先設定常用 port
+    port = 3000,
     app = express(),
     compiler = webpack(config),
     ngrokConfig = {
@@ -25,15 +24,16 @@ var
     });
 
 // if unset .env
-if( !~~dotenv.DEV_SERVER_PORT.length ) port = 3000 ;
+// port = 3000
+// if( !~~dotenv.DEV_SERVER_PORT.length )  ;
 
 // webpackDevConfig.waitUntilValid(function(){
 //   console.log('Package is in a valid state');
 // });
 
-app.use( webpackDevConfig );
+// app.use( webpackDevConfig );
 
-app.use(require('webpack-hot-middleware')(compiler));
+// app.use(require('webpack-hot-middleware')(compiler));
 app.use('/public', express.static('public'));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -53,15 +53,15 @@ process.on('uncaughtException', function( evt ) {
 app.listen( port , function(err) {
   process.stdout.write('\u001B[2J\u001B[0;0f');
   console.log('\x1b[32m','[Success]','\x1b[0m','Dev Server: Listening  ==>','\x1b[36m',port,'\x1b[0m','Port');
-  ngrok.connect( ngrokConfig,  function ( $err , url ) {
-    if ( !$err ) {
-      var slackPostMessage = require('./helper/slackPusher');
-      (dotenv.SLACK_DEMO_URL_PUSHER === 'true' && dotenv.SLACK_API_TOKEN) && slackPostMessage({token: dotenv.SLACK_API_TOKEN , url , channelID: 'U0EQTV0G3'});
-      console.log('\x1b[32m','[Success]','\x1b[0m','Ngrok Server: Demo URL Available  ==>','\x1b[36m', url ,'\x1b[0m');}
-    if (  $err ) {
-      console.log( 'error_code:' , $err.error_code )
-      console.log( $err.details.err );
-      ngrok.kill();
-    }
-  });
+  // ngrok.connect( ngrokConfig,  function ( $err , url ) {
+  //   if ( !$err ) {
+  //     var slackPostMessage = require('./helper/slackPusher');
+  //     (dotenv.SLACK_DEMO_URL_PUSHER === 'true' && dotenv.SLACK_API_TOKEN) && slackPostMessage({token: dotenv.SLACK_API_TOKEN , url , channelID: 'U0EQTV0G3'});
+  //     console.log('\x1b[32m','[Success]','\x1b[0m','Ngrok Server: Demo URL Available  ==>','\x1b[36m', url ,'\x1b[0m');}
+  //   if (  $err ) {
+  //     console.log( 'error_code:' , $err.error_code )
+  //     console.log( $err.details.err );
+  //     ngrok.kill();
+  //   }
+  // });
 });
